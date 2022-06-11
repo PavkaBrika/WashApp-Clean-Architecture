48f466911b28015515d1a0d0e09a3cf91adbcc6b
pavkabrika <pavlikbrichkin@mail.ru>
2022-06-12 04:04:33 +0500

    in process to add notifications

---
+++ app/src/main/java/com/breckneck/washappca/broadcastreceiver/NotificationReceiver.java
@@ -0,0 +1,24 @@
+package com.breckneck.washappca.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.breckneck.washappca.R;
+
+public class NotificationReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "test")
+                .setSmallIcon(R.drawable.ic_outline_add_circle_outline_24)
+                .setContentTitle("test")
+                .setContentText("content text")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(1, builder.build());
+    }
+}
